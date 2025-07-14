83a313235594143e05618ff283cbf93b2ceb2661
Yulonda Joy Wyatt <yulondawyatt@gmail.com>
2025-07-14 17:06:51 -0400

    Created MoviesList.jsx compoenent for Landing Page.

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function MoviesList() {
-  const [movieList, setMovieList] = useState ( [] );
+  const [moviesList, setMoviesList] = useState ([]);
 
-  const getMovies = () => {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/movies')
+      .then((res) => res.json())
+      .then((movieListData) => setMoviesList(movieListData))
+      .catch((err) => console.error(err));
+  }, []);
 
-  }
 
-  
   return (
-    <>
-      <h1>Movie Catalog</h1>
-    
-    
-    </>
+  
+    <div className="movieLine">
+      <h2>Movie Catalog</h2>
+      <ul>
+        {moviesList.map((movie) => (
+          <li key={movie.id}>
+            {movie.title} - {movie.genres}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
-
 }
