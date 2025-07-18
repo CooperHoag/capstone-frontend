77eb54b2ed2f1f19dd02e47cf60a071599741041
Yulonda Joy Wyatt <yulondawyatt@gmail.com>
2025-07-17 21:07:10 -0400

    Created movies-list.css and imported into MoviesList.jsx

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
+import "/src/movies-list.css";
 
 
 export default function MoviesList() {
@@ -29,22 +30,24 @@ export default function MoviesList() {
   });
 
   return (
-    <div className="movieLine">
-      <h2>Movie Catalog</h2>
-
-      <input
+    <div className="movie-list-component">
+      
+      <input 
+        className="movies-list-search"
         type="text"
         placeholder="Search by title or genre"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         style={{ marginBottom: "20px", padding: "6px", width: "25%" }}
       />
+      
+      <h2>Movie Catalog</h2>
 
       {!isAuthenticated && (
               <p style={{ color: 'gray' }}>Log in to view movie details</p>
             )}
 
-      <ul>
+      <ul className="list-of-movies">
         {filteredMovies.map((movie) => (
           <li key={movie.id}>
             {movie.title} - {movie.genres}
