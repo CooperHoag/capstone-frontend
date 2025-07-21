2445d0b5edf7ee8f07decbb0bc0190ea1bf7afdb
CooperHoag <cooperhoag@gmail.com>
2025-07-20 22:00:46 -0700

    Add css style to Navbar, AccountPage, and MovieDetails

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,26 +1,26 @@
 import { NavLink, useNavigate } from "react-router";
-
 import { useAuth } from "../auth/AuthContext";
+import "../stylesheets/Navbar.css"
 
 export default function Navbar() {
   const { token, logout } = useAuth();
   const navigate = useNavigate();
   return (
-    <header id="navbar">
-      <NavLink id="brand" to="/">
-        <p>Leisure Buddy</p>
+    <header className="navbar" id="navbar">
+      <NavLink className="navbar-logo" id="brand" to="/">
+        <p className="leisure-buddy-nb">Leisure Buddy</p>
       </NavLink>
-      <nav>
+      <nav className="navbar-links">
         {token ? (
           <>
-            <NavLink to="/movies">See All Movies</NavLink>
-            <NavLink to="/account">Account</NavLink>
-            <button onClick={() => { logout(); navigate("/"); }}>Log out</button>
+            <NavLink className="see-all-movies-nb" to="/movies">See All Movies</NavLink>
+            <NavLink className="account-nb" to="/account">Account</NavLink>
+            <button className="logout-nb" onClick={() => { logout(); navigate("/"); }}>Log out</button>
           </>
         ) : (
           <>
-            <NavLink to="/login">Log in</NavLink>
-            <NavLink to="/register">Sign Up</NavLink>
+            <NavLink className="login-nb" to="/login">Log in</NavLink>
+            <NavLink className="register-nb" to="/register">Sign Up</NavLink>
           </>
         )}
       </nav>
